ee3c0f5651b86356648be1c737ab59a4372fadbb
Gregory van Beek <gregoryvanbeek@tudelft.net>
2020-04-02 14:05:29 +0200

    Finished script for gene positions in 'chromosome_and_gene_positions,py'

--- Python_short_functions/chromosome_and_gene_positions.py
+++ Python_short_functions/chromosome_and_gene_positions.py
@@ -1,8 +1,15 @@
-
+'''This module includes two functions, 'chromosome_position' and 'gene_position'.
+Both do not require an input with the full path to the file 'Saccharomyces_cerevisiae.R64-1-1.99.gff3' (which can be downloaded from https://www.ensembl.org/Saccharomyces_cerevisiae/Info/Index).
+If no input is given, the program automatically searches for the file at r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Saccharomyces_cerevisiae.R64-1-1.99.gff3'.
+'''
 def chromosome_position(gff_file = None):
     '''Get the start and end position of each chromosome and determine their respective length.
     Input is a .gff file downloaded from https://www.ensembl.org/Saccharomyces_cerevisiae/Info/Index
     Output are three dictionaries for length, start and end position. All dictionaries have keys representing the chromosome number in roman numerals.
+    To get all dictionaries, use: 'a,b,c, chromosome_and_gene_position.chromosome_position()'.
+    'a' = chromosome length
+    'b' = chromosome start position
+    'c' = chromosome end position
     '''
     if gff_file == None:
         gff_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Saccharomyces_cerevisiae.R64-1-1.99.gff3'
@@ -42,7 +49,7 @@ def chromosome_position(gff_file = None):
             chr_start_pos_dict[roman] = 1
         counter += 1
 
-    return('chr_length_dict':chr_length_dict, 'chr_start_pos_dict':chr_start_pos_dict, 'chr_end_pos_dict':chr_end_pos_dict)
+    return(chr_length_dict, chr_start_pos_dict, chr_end_pos_dict)
 
 
 
@@ -50,18 +57,30 @@ def chromosome_position(gff_file = None):
 def gene_position(gff_file = None):
     '''Get the start and end position of each gene and determine their respective length.
     Input is a .gff file downloaded from https://www.ensembl.org/Saccharomyces_cerevisiae/Info/Index
+    Output is a dictionary that includes all gene names as keys. The values are lists with two numbers.
+    The first is the chromosome number the gene belong to, the second is the start position, the third is the end position of the gene in terms of basepairs.
     '''
 
     if gff_file == None:
         gff_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Saccharomyces_cerevisiae.R64-1-1.99.gff3'
 
-
+    gene_pos_dict = {}
     with open(gff_file) as f:
-        lines = f.readlines()
+        for line in f:
+            line_list = line.split('\t')
+            if len(line_list) > 2:
+                if line_list[2] == 'gene':
+                    gene_chr = line_list[0]
+                    gene_start = line_list[3]
+                    gene_end = line_list[4]
+                    gene_position = [gene_chr,gene_start,gene_end]
+                    
+                    gene_name_string = line_list[8].split(';')[0]
+                    gene_name = gene_name_string.split(':')[1]
+                    
+                    gene_pos_dict[gene_name] = gene_position
 
-
-
-
+    return(gene_pos_dict)
 
 if __name__ == '__main__':
     chromosome_position()
