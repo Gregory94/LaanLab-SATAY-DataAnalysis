8ab606de48b4c09388b0efefde272bffb44e0d7c
Gregory van Beek <gregoryvanbeek@tudelft.net>
2020-04-03 15:09:26 +0200

    Added fitness vs transposon density graph, updated python modules and updated ReadMe file for this branch.

--- python_modules/gene_names.py
+++ python_modules/gene_names.py
@@ -1,4 +1,5 @@
-   #CREATE A DICTIONARY WITH ALL GENES (BOTH THE COMMON NAME AND THEIR SYSTEMATIC NAME) AND SAVE THEM WITH THEIR RESPECTIVE LENGHTS (IN TERMS OF BP WHICH IS DEFINED AS bp=aa*3)
+'''
+'''
 
 def list_gene_names(gene_information_file = None):
     '''Create a list of all known gene names and their aliases as listed on SGD (or as provided as an optional input file)
@@ -7,7 +8,8 @@ def list_gene_names(gene_information_file = None):
     '''
 
     if gene_information_file == None:
-        gene_information_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Yeast_Protein_Names.txt'
+        gene_information_file = r'Yeast_Protein_Names.txt'
+        #gene_information_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Yeast_Protein_Names.txt'
 
     gene_name_list = []
     gene_counter = 0
@@ -42,20 +44,31 @@ def list_gene_names(gene_information_file = None):
 
 
 
-def list_gene_aliases(gene_information_file = None):
+def gene_aliases(gene_information_file = None):
+    '''Create three dictionaries containing aliases for genes
+    Input is the path to 'Protein_Names.txt' file downloaded from https://www.uniprot.org/docs/yeast.
+    If no input is given the file is automatically searched for at r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Yeast_Protein_Names.txt'.
+    Output is three dictionaries:
+    aliases_designation_dict = gene aliases for common names (e.g. Bem1 and Sro1)
+    aliases_sgd_dict = gene aliases for the search names in SGD (e.g. Bem1 and S000000404)
+    aliases_swissprot_dict = gene aliases for Swiss Prot.
+    The keys of the dictionaries are the systematic names of the genes (e.g. YBR200W for Bem1)
+    '''
 
     if gene_information_file == None:
-        gene_information_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Yeast_Protein_Names.txt'
+        gene_information_file = r'Yeast_Protein_Names.txt'
+        #gene_information_file = r'X:\tnw\BN\LL\Shared\Gregory\Gene_Database\Yeast_Protein_Names.txt'
 
 
-    gene_aliases_dict = {}
+    aliases_designation_dict = {}
+    aliases_sgd_dict = {}
+    aliases_swissprot_dict = {}
     
     
     with open(gene_information_file) as f:
         lines = f.readlines()
         
         for i in range(58,len(lines)-6):    #THE GENES START AT LINE 58 AND STOP 6 LINES BEFORE THE END OF THE FILE.
-            aliases_for_current_gene_list = []
             l = lines[i]
             l_list = l.split()
             
@@ -66,16 +79,24 @@ def list_gene_aliases(gene_information_file = None):
                 if names.endswith(';'):
                     gene_designation_list.append(names.strip(';'))
                     alias_counter += 1
-            gene_designation_list.append(names[alias_counter])
-            aliases_for_current_gene_list.append(gene_names_list)
+            gene_designation_list.append(l_list[alias_counter])
             
             
+            oln_name = l_list[alias_counter+1]
             sgd_name = l_list[alias_counter+4]
-            aliases_for_current_gene_list.append(sgd_name)
+            swissprot_name = l_list[alias_counter+2]
+            if oln_name == 'GAG' or oln_name == 'POL':
+                oln_name = l_list[alias_counter+2]
+                sgd_name = l_list[alias_counter+5]
+                swissprot_name = l_list[alias_counter+3]
+
+            aliases_designation_dict[oln_name] = gene_designation_list
+            aliases_sgd_dict[oln_name] = sgd_name
+            aliases_swissprot_dict[oln_name] = swissprot_name
 
 
-            gene_aliases_dict[alias_counter+1] = aliases_for_current_gene_list
 
+    return(aliases_designation_dict, aliases_sgd_dict, aliases_swissprot_dict)
 
 if __name__ == '__main__':
     list_gene_names()
