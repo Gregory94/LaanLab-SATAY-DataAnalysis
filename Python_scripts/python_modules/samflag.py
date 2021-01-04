39ff7056727df0490fff4755b5ab1abb535cd744
Gregory94 <g.m.vanbeek@tudelft.nl>
2021-01-04 12:19:30 +0100

    Added help text and print results. Fixed issue with return function that had to many tabs before.

--- Python_scripts/python_modules/samflag.py
+++ Python_scripts/python_modules/samflag.py
@@ -7,6 +7,9 @@ Created on Mon Jan  4 09:38:16 2021
 
 def samflag(flag=0):
     '''
+    This script converts a decimal flag to binary and get the corresponding properties according to the sam-flag standard.
+    The code is based on the explanation given here https://davetang.org/muse/2014/03/06/understanding-bam-flags/
+    The input is a decimal number.
     '''
 
     flag_binary = format(flag, '012b') # '#012b' to start the string with '0b'. 12 indicated that the string has length 12.
@@ -28,18 +31,7 @@ def samflag(flag=0):
                  10: 'read fails platform/vendor quality checks',
                  11: 'read is PCR or optical duplicate',
                  12: 'supplementary alignment'}
-#    prop1 = 'read paired'
-#    prop2 = 'read mapped in proper pair'
-#    prop3 = 'read unmapped'
-#    prop4 = 'mate unmapped'
-#    prop5 = 'read reverse strand'
-#    prop6 = 'mate reverse strand'
-#    prop7 = 'first in pair'
-#    prop8 = 'second in pair'
-#    prop9 = 'not primary alignment'
-#    prop10 = 'read fails platform/vendor quality checks'
-#    prop11 = 'read is PCR or optical duplicate'
-#    prop12 = 'supplementary alignment'
+
 
     counter = 1
     flagprop_list = []
@@ -47,11 +39,17 @@ def samflag(flag=0):
         if int(b) == 1:
             flagprop_list.append(prop_dict.get(counter))
         counter += 1
-        
-        return(flag_binary, flagprop_list)
+
+
+
+    return(flag_binary, flagprop_list)
 
 
 
 
 if __name__ == '__main__':
-    flag_binary, flagproperties = samflag(flag=2)
+    flag_binary, flagproperties = samflag(flag=1040)
+
+    print('PROPERTIES:')
+    for prop in flagproperties:
+        print(prop)
