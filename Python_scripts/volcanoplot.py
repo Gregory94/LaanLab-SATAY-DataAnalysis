bc3af66934839f0715721b90555318b055d11003
Gregory94 <g.m.vanbeek@tudelft.nl>
2021-02-19 10:23:41 +0100

    Update volcanoplot.py. Created dataframe with all statistics for creating volcanoplot

--- Python_scripts/volcanoplot.py
+++ Python_scripts/volcanoplot.py
@@ -3,6 +3,10 @@
 Created on Tue Feb 16 14:06:48 2021
 
 @author: gregoryvanbeek
+
+What we are doing when we do a t test is measuring the number of standard
+deviations our measured mean is from the baseline mean, while taking into
+account that the standard deviation of the mean can change as we get more data
 """
 
 import os, sys
@@ -37,74 +41,92 @@ del (files, datafile, datapath_a, datapath_b, filenames_a, filenames_b)
 
 
 
-#%% Get information from files and store in pandas dataframe
-#ADD COLUMN IN READPERINSRT_DF WITH THE STANDARD DEVIATION
-counter = 0
-for datafile_a in datafiles_list_a:
+#%% Get number of reads per insertion all datasets
+for count, datafile_a in enumerate(datafiles_list_a):
     read_gene_a = dataframe_from_pergenefile(datafile_a)
-    if counter == 0:
+    if count == 0:
         readsperinsrt_df = read_gene_a[['gene_names']] #initialize new dataframe with gene_names
-        readsperinsrt_df['mean_Nreadsperinsrt_a'] = read_gene_a[['Nreadsperinsrt']] #add column with Nreadsperinsrt
-        Nreadspersinsrt_a_array = read_gene_a[['Nreadsperinsrt']].to_numpy() #create numpy array to store raw data
+        Nreadsperinsrt_a_array = read_gene_a[['Nreadsperinsrt']].to_numpy() #create numpy array to store raw data
     else:
-        readsperinsrt_df['mean_Nreadsperinsrt_a'] = readsperinsrt_df['mean_Nreadsperinsrt_a'] + read_gene_a['Nreadsperinsrt'] #add values of Nreadsperinsrt to existing Nreadsperinsrt column
-        Nreadspersinsrt_a_array = np.append(Nreadspersinsrt_a_array, read_gene_a[['Nreadsperinsrt']].to_numpy(), axis=1) #append raw data
-    counter += 1
-readsperinsrt_df['mean_Nreadsperinsrt_a'] = readsperinsrt_df['mean_Nreadsperinsrt_a'].div(counter) #determine mean Nreadsperinsrt
-N_a = counter #save number of samples
+        Nreadsperinsrt_a_array = np.append(Nreadsperinsrt_a_array, read_gene_a[['Nreadsperinsrt']].to_numpy(), axis=1) #append raw data
+N_a = count+1 #save number of samples
 
 
 
-counter = 0
-for datafile_b in datafiles_list_b:
+for count, datafile_b in enumerate(datafiles_list_b):
     read_gene_b = dataframe_from_pergenefile(datafile_b)
-    if counter == 0:
-        readsperinsrt_df['mean_Nreadsperinsrt_b'] = read_gene_b[['Nreadsperinsrt']]
-        Nreadspersinsrt_b_array = read_gene_b[['Nreadsperinsrt']].to_numpy()
+    if count == 0:
+        Nreadsperinsrt_b_array = read_gene_b[['Nreadsperinsrt']].to_numpy()
     else:
-        readsperinsrt_df['mean_Nreadsperinsrt_b'] = readsperinsrt_df['mean_Nreadsperinsrt_b'] + read_gene_b['Nreadsperinsrt']
-        Nreadspersinsrt_b_array = np.append(Nreadspersinsrt_b_array, read_gene_b[['Nreadsperinsrt']].to_numpy(), axis=1)
-    counter += 1
-readsperinsrt_df['mean_Nreadsperinsrt_b'] = readsperinsrt_df['mean_Nreadsperinsrt_b'].div(counter)
-N_b = counter
+        Nreadsperinsrt_b_array = np.append(Nreadsperinsrt_b_array, read_gene_b[['Nreadsperinsrt']].to_numpy(), axis=1)
+N_b = count+1
+
+
+if not N_a == N_b:
+    print("WARNING: Length of dataset a is NOT the same as the length of dataset b.")
+N = count+1
+
+del (datafile_a, datafile_b, count, read_gene_a, read_gene_b, N_a, N_b)
 
 
 
-del (datafile_a, datafile_b, counter, read_gene_a, read_gene_b)
 
+#%% Determine mean and p value of reads per insertion for each gene
+df = 2*N - 2 #degrees of freedom
 
-#%% Get information from files and store in pandas dataframe BACKUP
-#IN CASE OF WARNING: https://towardsdatascience.com/explaining-the-settingwithcopywarning-in-pandas-ebc19d799d25 . Add .copy() after .loc[...]
-# counter = 0
-# for datafile_a in datafiles_list_a:
-#     read_gene_a = dataframe_from_pergenefile(datafile_a)
-#     if counter == 0:
-#         readsperinsrt_df = read_gene_a[['gene_names']]
-#         readsperinsrt_mean_array = read_gene_a[['Nreadsperinsrt']].to_numpy()
-#     readsperinsrt_df['mean_Nreadsperinsrt_a'+str(counter)] = read_gene_a[['Nreadsperinsrt']]
-#     readsperinsrt_mean_array = np.add(read_gene_a[['Nreadsperinsrt']].to_numpy(), readsperinsrt_mean_array)
-#     if counter == len(datafiles_list_a):
-#         readsperinsrt_mean_array = readsperinsrt_mean_array / np.array(counter)
-#     counter += 1
+mean_Nreadsperinsrt_a_list = [np.nan]*len(Nreadsperinsrt_a_array)
+mean_Nreadsperinsrt_b_list = [np.nan]*len(Nreadsperinsrt_b_array)
+var_Nreadsperinsrt_a_list = [np.nan]*len(Nreadsperinsrt_a_array)
+var_Nreadsperinsrt_b_list = [np.nan]*len(Nreadsperinsrt_b_array)
 
+for ii in range(len(Nreadsperinsrt_a_array)): #determine mean and variance
+    mean_Nreadsperinsrt_a_list[ii] = Nreadsperinsrt_a_array[ii].mean()
+    mean_Nreadsperinsrt_b_list[ii] = Nreadsperinsrt_b_array[ii].mean()
 
-# counter = 0
-# for datafile_b in datafiles_list_b:
-#     read_gene_b = dataframe_from_pergenefile(datafile_b)
-#     readsperinsrt_df['mean_Nreadsperinsrt_b'+str(counter)] = read_gene_b[['Nreadsperinsrt']]
-#     counter += 1
-
-
-# del (datafile_a, datafile_b, read_gene_a, read_gene_b)
-
-
-#%% Define fold change and create new dataframe.
-#IN CASE OF WARNING: https://towardsdatascience.com/explaining-the-settingwithcopywarning-in-pandas-ebc19d799d25 . Add .copy() after .loc[...]
-# volcano_df = readsperinsrt_df[['gene_names']]
-# volcano_df['fold_change'] = readsperinsrt_df - readsperinsrt_df
+    #For unbiased max likelihood estimate we have to divide the var by N-1, and therefore the parameter ddof = 1
+    var_Nreadsperinsrt_a_list[ii] = Nreadsperinsrt_a_array[ii].var(ddof=1)
+    var_Nreadsperinsrt_b_list[ii] = Nreadsperinsrt_b_array[ii].var(ddof=1)
 
 
 
+std_Nreadsperinsrt_list = [np.nan]*len(Nreadsperinsrt_a_array)
+tstat_Nreadsperinsrt_list = [np.nan]*len(Nreadsperinsrt_a_array)
+p_Nreadsperinsrt_list = [np.nan]*len(Nreadsperinsrt_a_array)
+
+for ss in range(len(var_Nreadsperinsrt_a_list)): #determine standard deviation and t statistic
+    std_Nreadsperinsrt_list[ss] = np.sqrt((var_Nreadsperinsrt_a_list[ss] + var_Nreadsperinsrt_b_list[ss])/2)
+    if std_Nreadsperinsrt_list[ss] == 0.0:
+        tstat_Nreadsperinsrt_list[ss] = 0
+    else:
+        tstat_Nreadsperinsrt_list[ss] = (mean_Nreadsperinsrt_a_list[ss] - mean_Nreadsperinsrt_b_list[ss]) / (std_Nreadsperinsrt_list[ss] * np.sqrt(2/N))
+    p_Nreadsperinsrt_list[ss] = 1 - stats.t.cdf(tstat_Nreadsperinsrt_list[ss], df=df)
+
+readsperinsrt_df['mean_Nreadsperinsrt_a'] = mean_Nreadsperinsrt_a_list
+readsperinsrt_df['mean_Nreadsperinsrt_b'] = mean_Nreadsperinsrt_b_list
+readsperinsrt_df['std_Nreadsperinsrt'] = std_Nreadsperinsrt_list
+readsperinsrt_df['tstat_Nreadsperinsrt'] = tstat_Nreadsperinsrt_list
+readsperinsrt_df['pval_Nreadsperinsrt'] = p_Nreadsperinsrt_list
+
+
+del (mean_Nreadsperinsrt_a_list, mean_Nreadsperinsrt_b_list, var_Nreadsperinsrt_a_list, var_Nreadsperinsrt_b_list, ii, ss,
+     std_Nreadsperinsrt_list, tstat_Nreadsperinsrt_list, p_Nreadsperinsrt_list)
+
+
+#%% Determine fold change of mean_Nreadsperinsrt
+fc_list = [np.nan]*len(readsperinsrt_df) #initialize list for storing fold changes
+
+for count, avg in enumerate(readsperinsrt_df.itertuples()):
+    if not avg.mean_Nreadsperinsrt_b == 0:
+        fc_list[count] = np.log2(avg.mean_Nreadsperinsrt_a / avg.mean_Nreadsperinsrt_b) #DIVIDE DATASET A BY DATASET B
+    else:
+        fc_list[count] = 0
+
+readsperinsrt_df['log2_fold_change'] = fc_list #add fc_list to dataframe
+
+del (avg, count, fc_list)
+
+
+#%% Volcanoplot
 
 
 
