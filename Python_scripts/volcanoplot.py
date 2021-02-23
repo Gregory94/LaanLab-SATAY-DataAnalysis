1484d900eff7025c1585389675f6acb33763c4e3
Gregory94 <g.m.vanbeek@tudelft.nl>
2021-02-23 15:06:39 +0100

    Tests with different ways of normalization and fodl change calculations

--- Python_scripts/volcanoplot.py
+++ Python_scripts/volcanoplot.py
@@ -42,9 +42,9 @@ filenames_b = ["dnrp1-1-a_pergene.txt", "dnrp1-1-b_pergene.txt", "dnrp1-2-a_perg
 
 variable = 'read_per_gene' #'read_per_gene' 'tn_per_gene', 'Nreadsperinsrt'
 significance_threshold = 0.01 #set threshold above which p-values are regarded significant
-normalize=False
+normalize=True
 
-track_gene = "ALG1"# "CDC42" or set to "" to disable (set for debugging)
+track_gene = "VTI1"# "CDC42" or set to "" to disable (set for debugging)
 
 
 #%% Check files
@@ -70,45 +70,45 @@ norm_a = 0
 norm_b = 0
 for count, datafile_a in enumerate(datafiles_list_a):
     tnread_gene_a = dataframe_from_pergenefile(datafile_a, verbose=False)
-    # if normalize == True:
-    #     if variable == 'tn_per_gene':
-    #         norm_a = sum(tnread_gene_a.tn_per_gene)*10**-4
-    #     elif variable == 'read_per_gene':
-    #         norm_a = sum(tnread_gene_a.read_per_gene)*10**-7
-    norm_a += sum(tnread_gene_a.tn_per_gene)
+    if normalize == True:
+        if variable == 'tn_per_gene':
+            norm_a = sum(tnread_gene_a.tn_per_gene)#*10**-4
+        elif variable == 'read_per_gene':
+            norm_a = sum(tnread_gene_a.read_per_gene)#*10**-7
+    # norm_a += sum(tnread_gene_a.tn_per_gene)
 
     if count == 0:
         volcano_df = tnread_gene_a[['gene_names']] #initialize new dataframe with gene_names
-        # if normalize == True:
-        #     variable_a_array = np.divide(tnread_gene_a[[variable]].to_numpy(), norm_a) #create numpy array to store raw data
-        # else:
-        variable_a_array = tnread_gene_a[[variable]].to_numpy()
+        if normalize == True:
+            variable_a_array = np.divide(tnread_gene_a[[variable]].to_numpy(), norm_a) #create numpy array to store raw data
+        else:
+            variable_a_array = tnread_gene_a[[variable]].to_numpy()
     else:
-        # if normalize == True:
-        #     variable_a_array = np.append(variable_a_array, np.divide(tnread_gene_a[[variable]].to_numpy(), norm_a), axis=1) #append raw data
-        # else:
-        variable_a_array = np.append(variable_a_array, tnread_gene_a[[variable]].to_numpy(), axis=1)
+        if normalize == True:
+            variable_a_array = np.append(variable_a_array, np.divide(tnread_gene_a[[variable]].to_numpy(), norm_a), axis=1) #append raw data
+        else:
+            variable_a_array = np.append(variable_a_array, tnread_gene_a[[variable]].to_numpy(), axis=1)
 
 
 for count, datafile_b in enumerate(datafiles_list_b):
     tnread_gene_b = dataframe_from_pergenefile(datafile_b, verbose=False)
-    # if normalize == True:
-    #     if variable == 'tn_per_gene':
-    #         norm_b = sum(tnread_gene_b.tn_per_gene)*10**-4
-    #     elif variable == 'read_per_gene':
-    #         norm_b = sum(tnread_gene_b.read_per_gene)*10**-7
-    norm_b += sum(tnread_gene_b.tn_per_gene)
+    if normalize == True:
+        if variable == 'tn_per_gene':
+            norm_b = sum(tnread_gene_b.tn_per_gene)#*10**-4
+        elif variable == 'read_per_gene':
+            norm_b = sum(tnread_gene_b.read_per_gene)#*10**-7
+    # norm_b += sum(tnread_gene_b.tn_per_gene)
 
     if count == 0:
-        # if normalize == True:
-        #     variable_b_array = np.divide(tnread_gene_b[[variable]].to_numpy(), norm_b)
-        # else:
-        variable_b_array = tnread_gene_b[[variable]].to_numpy()
+        if normalize == True:
+            variable_b_array = np.divide(tnread_gene_b[[variable]].to_numpy(), norm_b)
+        else:
+            variable_b_array = tnread_gene_b[[variable]].to_numpy()
     else:
-        # if normalize == True:
-        #     variable_b_array = np.append(variable_b_array, np.divide(tnread_gene_b[[variable]].to_numpy(), norm_b), axis=1)
-        # else:
-        variable_b_array = np.append(variable_b_array, tnread_gene_b[[variable]].to_numpy(), axis=1)
+        if normalize == True:
+            variable_b_array = np.append(variable_b_array, np.divide(tnread_gene_b[[variable]].to_numpy(), norm_b), axis=1)
+        else:
+            variable_b_array = np.append(variable_b_array, tnread_gene_b[[variable]].to_numpy(), axis=1)
 
 
 ### printing specific genes
@@ -140,25 +140,20 @@ for count, val in enumerate(variable_a_array):
         signif_thres_list[count] = True
 
     #Take mean of number of insertions per library
-    # if np.mean(variable_b_array[count]) == 0 and np.mean(variable_a_array[count]) == 0:
-    #     fc_list[count] = 0
-    # elif np.mean(variable_b_array[count]) == 0 or np.mean(variable_a_array[count]) == 0:
-    #     fc_list[count] = np.log2(max(np.mean(variable_a_array[count]), np.mean(variable_b_array[count])))
-    # else:
-    #     fc_list[count] = np.log2(np.mean(variable_b_array[count]) / np.mean(variable_a_array[count]))
+    if np.mean(variable_b_array[count]) == 0 and np.mean(variable_a_array[count]) == 0:
+        fc_list[count] = 0
+    elif np.mean(variable_b_array[count]) == 0 or np.mean(variable_a_array[count]) == 0:
+        fc_list[count] = np.log2(max(np.mean(variable_a_array[count]), np.mean(variable_b_array[count])))
+    else:
+        fc_list[count] = np.log2(np.mean(variable_b_array[count]) / np.mean(variable_a_array[count]))
 
     #Take sum of number of insertions per library 
-    # if sum(variable_a_array[count]) == 0 or sum(variable_b_array[count]) == 0:
-    #     if sum(variable_a_array[count]) >= sum(variable_b_array[count]):
-    #         fc_list[count] = np.log2((sum(variable_a_array[count]) / norm_a))
-    #     else:
-    #         fc_list[count] = np.log2((sum(variable_b_array[count]) / norm_b))
-    if sum(variable_a_array[count]) == 0 and sum(variable_b_array[count]) == 0:
-        fc_list[count] = 0
-    elif sum(variable_a_array[count]) == 0 or sum(variable_b_array[count]) == 0:
-        fc_list[count] = np.log2(max(sum(variable_a_array[count]) / norm_a, sum(variable_b_array[count]) / norm_b))
-    else:
-        fc_list[count] = np.log2((sum(variable_a_array[count]) / norm_a) / (sum(variable_b_array[count]) / norm_b))
+    # if sum(variable_a_array[count]) == 0 and sum(variable_b_array[count]) == 0:
+    #     fc_list[count] = 0
+    # elif sum(variable_a_array[count]) == 0 or sum(variable_b_array[count]) == 0:
+    #     fc_list[count] = np.log2(max(sum(variable_a_array[count]) / norm_a, sum(variable_b_array[count]) / norm_b))
+    # else:
+    #     fc_list[count] = np.log2((sum(variable_a_array[count]) / norm_a) / (sum(variable_b_array[count]) / norm_b))
 
 
     ### printing specific genes
@@ -176,9 +171,9 @@ volcano_df['t_statistic'] = ttest_tval_list
 volcano_df['p_value'] = ttest_pval_list
 volcano_df['significance'] = signif_thres_list
 
-del(count, val, ttest_val, ttest_tval_list, ttest_pval_list, fc_list, signif_thres_list, track_gene, norm_a, norm_b)
-# if normalize == True:
-#     del (norm_a, norm_b)
+del(count, val, ttest_val, ttest_tval_list, ttest_pval_list, fc_list, signif_thres_list, track_gene)
+if normalize == True:
+    del (norm_a, norm_b)
 
 
 #%% Volcanoplot
@@ -188,8 +183,8 @@ grid = plt.GridSpec(1, 1, wspace=0.0, hspace=0.0)
 ax = plt.subplot(grid[0,0])
 
 # ax.scatter(x=volcano_df.fold_change, y=volcano_df.p_value, alpha=0.4, marker='.', c='k')
-ax.scatter(x=volcano_df.loc[volcano_df['significance'] == False, 'fold_change'], y=volcano_df.loc[volcano_df['significance'] == False, 'p_value'], alpha=0.4, marker='.', c='k', label='p-value < {}'.format(significance_threshold))
-ax.scatter(x=volcano_df.loc[volcano_df['significance'] == True, 'fold_change'], y=volcano_df.loc[volcano_df['significance'] == True, 'p_value'], alpha=0.4, marker='.', c='r', label='p-value > {}'.format(significance_threshold))
+ax.scatter(x=volcano_df.loc[volcano_df['significance'] == False, 'fold_change'], y=volcano_df.loc[volcano_df['significance'] == False, 'p_value'], alpha=0.4, marker='.', c='k', label='p-value > {}'.format(significance_threshold))
+ax.scatter(x=volcano_df.loc[volcano_df['significance'] == True, 'fold_change'], y=volcano_df.loc[volcano_df['significance'] == True, 'p_value'], alpha=0.4, marker='.', c='r', label='p-value < {}'.format(significance_threshold))
 ax.grid(True, which='major', axis='both', alpha=0.4)
 ax.set_xlabel('Log2 FC')
 ax.set_ylabel('-1*Log10 p-value')
