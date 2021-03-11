e6ff4afbe5d926e00d878a574d5c735b3a5b0221
Gregory94 <g.m.vanbeek@tudelft.nl>
2021-03-11 10:51:25 +0100

    Added option to search with aliases to find differences between two pergene.txt files

--- Python_scripts/Compare_datasets/compare_pergene_files.py
+++ Python_scripts/Compare_datasets/compare_pergene_files.py
@@ -5,6 +5,10 @@ Created on Wed Mar 10 14:38:47 2021
 @author: gregoryvanbeek
 """
 import os
+from gene_names import gene_aliases
+
+gene_alias_dict = gene_aliases(r"C:\Users\gregoryvanbeek\Documents\GitHub\LaanLab-SATAY-DataAnalysis\Data_Files\Yeast_Protein_Names.txt")[0]
+
 
 filepath_a = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\dataset_leila\dataset_leila_wt\leila_dataset_wt_processing\WT_merged-techrep-a_techrep-b_processing\WT_merged-techrep-a_techrep-b_trimmed.sorted.bam_pergene.txt"
 filepath_b = r"C:\Users\gregoryvanbeek\Documents\Data_Sets\dataset_leila\WT_pergene.txt"
@@ -45,3 +49,22 @@ for gene in genes_b:
         genes_in_b_not_in_a.append(gene)
 
 del (gene)
+
+
+#check for aliases
+for index, gene in enumerate(genes_in_a_not_in_b):
+    alias_list = []
+    for keys, nums in gene_alias_dict.items():
+        if keys == gene or gene in nums:
+            alias_list = gene_alias_dict.get(keys)
+            alias_list.append(keys)
+            break
+
+    if not alias_list == []:
+        for alias in alias_list:
+            if alias in genes_in_b_not_in_a:
+                genes_in_b_not_in_a.remove(alias)
+                break
+
+
+# del (keys, nums, gene, alias_list, index, alias)
